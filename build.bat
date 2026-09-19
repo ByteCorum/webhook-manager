8da68ab7667a96b6efd96e9aee37c5631a766ef2
ByteCorum <164874887+ByteCorum@users.noreply.github.com>
2026-09-19 23:53:34 +0200

    chore: structure

---
+++ build.bat
@@ -0,0 +1,2 @@
+python.exe -m nuitka --quiet --remove-output --onefile --follow-imports --windows-icon-from-ico=resources/icon.ico --output-filename=WebhookController --enable-plugin=tk-inter main.py
+pause null
--- pack.bat
+++
@@ -1,2 +0,0 @@
-python.exe -m nuitka --quiet --remove-output --onefile --follow-imports --windows-icon-from-ico=resources/icon.ico --output-filename=WebhookController --enable-plugin=tk-inter main.py
-pause null
