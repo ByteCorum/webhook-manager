a30f1b3c496278b8a08652b9a291e37052eca625
ByteCorum <164874887+ByteCorum@users.noreply.github.com>
2026-09-21 17:05:44 +0200

    feat: add cross-platform support

--- build.bat
+++
@@ -1,2 +0,0 @@
-python.exe -m nuitka --quiet --remove-output --onefile --follow-imports --windows-icon-from-ico=assets/program/icon.ico --output-filename=webhook-controller --enable-plugin=tk-inter src/main.py
-pause null
