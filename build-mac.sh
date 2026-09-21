105602d681204ee9585cab4321962e171e8c9327
ByteCorum <164874887+ByteCorum@users.noreply.github.com>
2026-09-21 17:05:20 +0200

    feat: add cross-platform support

---
+++ build-mac.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+python.exe -m nuitka \
+    --follow-imports \
+    --remove-output \
+    --assume-yes-for-downloads \
+    --onefile \
+    --output-filename=webhook-controller \
+    --enable-plugin=tk-inter \
+    --company-name="ByteCorum" \
+    --product-name="webhook-controller" \
+    --file-version=1.0.0.5 \
+    --product-version=1.0.0.5 \
+    --file-description="Python GUI cross-platform prigram for controlling discord webhooks" \
+    --copyright="https://github.com/ByteCorum/WebhookController/blob/main/LICENSE" \
+    src/main.py
